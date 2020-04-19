cecb63233c22598ef18dd0b0d59b504cf56f4604
Kurbezz <kurbanovbul@gmail.com>
2020-04-19 13:49:44 +0300

    Remove unsed code

--- source/sql/create_download_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS download
-(
-    book_id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL,
-    last DATE NOT NULL,
-    PRIMARY KEY (book_id, user_id)
-);
-ALTER TABLE download OWNER TO {};
